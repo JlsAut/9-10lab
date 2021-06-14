34d3a2a802d5c7997b16fb7b2a63ba5cdba7762a
JlsAut <70904475+JlsAut@users.noreply.github.com>
2021-06-14 14:14:09 +0300

    Create MainPage.jsx

---
+++ src/components/MainPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const MainPage = () => {
+  return <div className="mainPage">Sign In for free</div>;
+};
+
+export default MainPage;import React from "react";
+
+const MainPage = () => {
+  return <div className="mainPage">Sign In for free</div>;
+};
+
+export default MainPage;
